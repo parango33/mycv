20a25940ac3d1f092792890c27bd2bec4bd09bd9
parango33 <60206994+parango33@users.noreply.github.com>
2023-03-12 18:51:07 -0500

    modify js file name and port

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use('/css', express.static(path.join(__dirname, 'css')));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 
-const port = 5500;
+const port = 8081;
 
 
 app.get('/',(req,res) => {
